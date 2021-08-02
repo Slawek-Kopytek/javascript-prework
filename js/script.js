9765ff2a618700b419d19a33f3d09b57a2b94c52
Sławek Kopytek <slavkopytek@gmail.com>
2021-08-02 07:43:58 +0200

    change let to const

--- js/script.js
+++ js/script.js
@@ -9,14 +9,13 @@
         const argComputerMove = getMoveName(randomNumber);
 
         function getMoveName(number) {
-    
-        if (number == 1) {
-            return 'kamień';
-        } else if (number == 2) {
-            return 'papier';
-        } else if (number == 3) {
-            return 'nożyce';
-        }
+            if (number == 1) {
+                return 'kamień';
+            } else if (number == 2) {
+                return 'papier';
+            } else if (number == 3) {
+                return 'nożyce';
+            }
         }
         printMessage('Komputer zagrał: ' + argComputerMove);
   
@@ -26,28 +25,27 @@
 
         function displayResult(argComputerMove, argPlayerMove) {
         console.log('moves:', argComputerMove, argPlayerMove);
-    
-        if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-            printMessage('Wygrywasz!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-            printMessage('Wygrywasz!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-            printMessage('Wygrywasz!');
-        } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-            printMessage('Przegrywasz!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-            printMessage('Przegrywasz!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-            printMessage('Przegrywasz!');
-        } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
-            printMessage('Remis!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
-            printMessage('Remis!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
-            printMessage('Remis!');
-        } else {
-            printMessage('Błąd!');
-        }
+            if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+                printMessage('Wygrywasz!');
+            } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+                printMessage('Wygrywasz!');
+            } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+                printMessage('Wygrywasz!');
+            } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+                printMessage('Przegrywasz!');
+            } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+                printMessage('Przegrywasz!');
+            } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+                printMessage('Przegrywasz!');
+            } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+                printMessage('Remis!');
+            } else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
+                printMessage('Remis!');
+            } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+                printMessage('Remis!');
+            } else {
+                printMessage('Błąd!');
+            }
         }
     }
     document.getElementById('play-rock').addEventListener('click', function () {
